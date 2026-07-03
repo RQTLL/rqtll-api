d2af2782c7f21c8bb582edb43497a05f3f4b23d9
adnksharp <darkmark404@protonmail.com>
2026-07-03 16:27:35 -0600

    feat: filter for listed packages

--- py/packages_pb2.py
+++ py/packages_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0epackages.proto\x12\x0brqt2.api.v1\"%\n\x13ListPackagesRequest\x12\x0e\n\x06\x66ilter\x18\x01 \x01(\t\"W\n\x0bPackageInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x14\n\x0cis_installed\x18\x04 \x01(\x08\"&\n\x0eInstallRequest\x12\x14\n\x0cpackage_name\x18\x01 \x01(\t\"5\n\x0fInstallProgress\x12\x10\n\x08log_line\x18\x01 \x01(\t\x12\x10\n\x08progress\x18\x02 \x01(\x02\x32\xb6\x01\n\x0ePackageService\x12U\n\x15ListAvailablePackages\x12 .rqt2.api.v1.ListPackagesRequest\x1a\x18.rqt2.api.v1.PackageInfo0\x01\x12M\n\x0eInstallPackage\x12\x1b.rqt2.api.v1.InstallRequest\x1a\x1c.rqt2.api.v1.InstallProgress0\x01\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0epackages.proto\x12\x0brqt2.api.v1\"^\n\x13ListPackagesRequest\x12\x0e\n\x06\x66ilter\x18\x01 \x01(\t\x12\x10\n\x08show_ros\x18\x02 \x01(\x08\x12\x13\n\x0bshow_python\x18\x03 \x01(\x08\x12\x10\n\x08show_rti\x18\x04 \x01(\x08\"W\n\x0bPackageInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x14\n\x0cis_installed\x18\x04 \x01(\x08\"&\n\x0eInstallRequest\x12\x14\n\x0cpackage_name\x18\x01 \x01(\t\"5\n\x0fInstallProgress\x12\x10\n\x08log_line\x18\x01 \x01(\t\x12\x10\n\x08progress\x18\x02 \x01(\x02\x32\xb6\x01\n\x0ePackageService\x12U\n\x15ListAvailablePackages\x12 .rqt2.api.v1.ListPackagesRequest\x1a\x18.rqt2.api.v1.PackageInfo0\x01\x12M\n\x0eInstallPackage\x12\x1b.rqt2.api.v1.InstallRequest\x1a\x1c.rqt2.api.v1.InstallProgress0\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,13 +32,13 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'packages_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_LISTPACKAGESREQUEST']._serialized_start=31
-  _globals['_LISTPACKAGESREQUEST']._serialized_end=68
-  _globals['_PACKAGEINFO']._serialized_start=70
-  _globals['_PACKAGEINFO']._serialized_end=157
-  _globals['_INSTALLREQUEST']._serialized_start=159
-  _globals['_INSTALLREQUEST']._serialized_end=197
-  _globals['_INSTALLPROGRESS']._serialized_start=199
-  _globals['_INSTALLPROGRESS']._serialized_end=252
-  _globals['_PACKAGESERVICE']._serialized_start=255
-  _globals['_PACKAGESERVICE']._serialized_end=437
+  _globals['_LISTPACKAGESREQUEST']._serialized_end=125
+  _globals['_PACKAGEINFO']._serialized_start=127
+  _globals['_PACKAGEINFO']._serialized_end=214
+  _globals['_INSTALLREQUEST']._serialized_start=216
+  _globals['_INSTALLREQUEST']._serialized_end=254
+  _globals['_INSTALLPROGRESS']._serialized_start=256
+  _globals['_INSTALLPROGRESS']._serialized_end=309
+  _globals['_PACKAGESERVICE']._serialized_start=312
+  _globals['_PACKAGESERVICE']._serialized_end=494
 # @@protoc_insertion_point(module_scope)
